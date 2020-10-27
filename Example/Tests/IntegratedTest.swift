477e067da6614560ba1bce8a046405ef0685f629
nayanda <nayanda1@outlook.com>
2020-10-27 18:50:10 +0700

    Added Test for aggregate

--- Example/Tests/IntegratedTest.swift
+++ Example/Tests/IntegratedTest.swift
@@ -233,6 +233,34 @@ class IntegratedTestSpec: QuickSpec {
             expect(obj.body).to(equal("bar"))
             expect(obj.completed).to(beNil())
         }
+        it("should aggregate request") {
+            let firstRealUrl: URL = try! "https://jsonplaceholder.typicode.com/todos/\(Int.random(in: 0..<10))".asUrl()
+            let secondRealUrl: URL = try! "https://jsonplaceholder.typicode.com/todos/\(Int.random(in: 10..<20))".asUrl()
+            let firstRequest = Ness.default
+                .httpRequest(.get, withUrl: firstRealUrl)
+                .prepareDataRequest()
+            let secondRequest = Ness.default
+                .httpRequest(.get, withUrl: secondRealUrl)
+                .prepareDataRequest()
+            var firstResult: URLResult?
+            var secondResult: URLResult?
+            waitUntil(timeout: 15) { done in
+                firstRequest.aggregate(with: secondRequest).then { result in
+                    expect(result.areCompleted).to(beTrue())
+                    expect(result.results.count).to(equal(2))
+                    firstResult = result.results[0]
+                    secondResult = result.results[1]
+                    done()
+                }
+            }
+            let firstUrl = try! firstResult?.httpMessage?.url.asUrl()
+            let secondUrl = try! secondResult?.httpMessage?.url.asUrl()
+            expect(firstUrl).toNot(beNil())
+            expect(secondUrl).toNot(beNil())
+            expect(firstUrl).toNot(equal(secondUrl))
+            expect(firstUrl == firstRealUrl || firstUrl == secondRealUrl).to(beTrue())
+            expect(secondUrl == firstRealUrl || secondUrl == secondRealUrl).to(beTrue())
+        }
     }
 }
 
