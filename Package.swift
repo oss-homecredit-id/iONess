c004aa61fbb5f06f7b15461359090918d70fd0c8
Nayanda Haberty <nayanda1@outlook.com>
2021-02-19 15:21:41 +0700

    Revert foldering and added macOS and tvOS

--- Package.swift
+++ Package.swift
@@ -6,26 +6,31 @@ import PackageDescription
 let package = Package(
     name: "iONess",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v10)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "iONess",
-            targets: ["iONess"]),
+            targets: ["iONess"]
+        )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iONess",
-            dependencies: []),
+            dependencies: [],
+            path: "iONess/Classes"
+        ),
         .testTarget(
             name: "iONessTests",
-            dependencies: ["iONess"]),
+            dependencies: ["iONess", "Quick", "Nimble"],
+            path: "Example/Tests",
+            exclude: ["Info.plist"]
+        )
     ]
 )
--- iONess.podspec
+++ iONess.podspec
@@ -30,7 +30,7 @@ Pod::Spec.new do |s|
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'Sources/iONess/Classes/**/*'
+  s.source_files = 'iONess/Classes/**/*'
   
   # s.resource_bundles = {
   #   'iONess' => ['iONess/Assets/*.png']
