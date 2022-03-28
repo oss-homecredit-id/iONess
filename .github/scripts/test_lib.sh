0b767ca6cee9743da287b74296959c628959df95
Nayanda Haberty <hainayanda@outlook.com>
2022-03-28 09:23:44 +0700

    Fix CI

--- .github/scripts/test_lib.sh
+++ .github/scripts/test_lib.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace Example/iONess.xcworkspace \
             -scheme iONess-Example \
-            -destination platform=iOS\ Simulator,OS=14.3,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
             clean test | xcpretty
